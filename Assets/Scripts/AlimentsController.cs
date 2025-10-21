4750a74be6505956065fe2a209be372fa0a8b4a0
AlexandreBeraud <beraud.mailpro@gmail.com>
2025-10-21 16:27:37 +0200

    MERGE SYSTEM

--- Assets/Scripts/AlimentsController.cs
+++ Assets/Scripts/AlimentsController.cs
@@ -1,35 +1,73 @@
-using System;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class AlimentsController : MonoBehaviour
 {
+    [SerializeField] private AlimentsScriptableObject alimentsData;
+    [SerializeField] private int alimentIndex;
     [SerializeField] private BoxCollider boxCollider;
-    
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private bool hasMerged = false;
 
     private void OnCollisionEnter(Collision other)
     {
-        Debug.Log(other.gameObject.name);
-        Merge(other);
+        if (hasMerged) return;
+
+        AlimentsController otherAliment = other.gameObject.GetComponent<AlimentsController>();
+        if (otherAliment == null || otherAliment.hasMerged) return;
+
+        TryMerge(otherAliment);
     }
 
-    private void Merge(Collision collision)
+    private void TryMerge(AlimentsController otherAliment)
     {
-        if (this.gameObject.CompareTag(collision.gameObject.tag))
+        if (alimentIndex == otherAliment.alimentIndex)
         {
-            Debug.Log("Two same elements interact");
+            hasMerged = true;
+            otherAliment.hasMerged = true;
+
+            Debug.Log($"Merge detected : index {alimentIndex}");
+
+            int nextIndex = alimentIndex + 1;
+            var nextAliment = alimentsData.GetAlimentByIndex(nextIndex);
+
+            if (nextAliment == null)
+            {
+                Debug.Log($"No next item {nextIndex}.");
+                return;
+            }
+
+            var currentAliment = alimentsData.GetAlimentByIndex(alimentIndex);
+            if (currentAliment?.DestroySound != null)
+                AudioSource.PlayClipAtPoint(currentAliment.DestroySound, transform.position);
+
+            Vector3 spawnPos = (transform.position + otherAliment.transform.position) / 2f;
+
+            GameObject newObj = Instantiate(nextAliment.Prefab, spawnPos, Quaternion.identity);
+
+            AlimentsController newController = newObj.GetComponent<AlimentsController>();
+            if (newController != null)
+            {
+                newController.SetIndex(nextAliment.Index);
+                newController.SetAlimentsData(alimentsData);
+            }
+
+            if (nextAliment.SpawnSound != null)
+                AudioSource.PlayClipAtPoint(nextAliment.SpawnSound, spawnPos);
+
+            Destroy(gameObject);
+            Destroy(otherAliment.gameObject);
+
+            Debug.Log($"Merged : {nextAliment.Name} créé !");
         }
+    }
+
+    public void SetIndex(int index)
+    {
+        alimentIndex = index;
+    }
+
+    public void SetAlimentsData(AlimentsScriptableObject data)
+    {
+        alimentsData = data;
     }
 }
--- Assets/Scripts/SpawnObjecsScript.cs
+++ Assets/Scripts/SpawnObjecsScript.cs
@@ -5,17 +5,23 @@ public class SpawnObjecsScript : MonoBehaviour
     [SerializeField] private AlimentsScriptableObject aliments;
     [SerializeField] private KeyCode spawnKey = KeyCode.Space;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(spawnKey))
         {
-            AlimentsScriptableObject.Aliment alimentToSpawn = aliments.GetAlimentByIndex(Random.Range(0, aliments.Aliments.Count));
-            //AlimentsScriptableObject.Aliment alimentToSpawn = aliments.GetAlimentByIndex(5);
+            int randomIndex = Random.Range(0, aliments.Aliments.Count);
+            var alimentToSpawn = aliments.GetAlimentByIndex(randomIndex);
 
-            
-            Instantiate(alimentToSpawn.Prefab, transform.position, transform.rotation);
-            Debug.Log(alimentToSpawn.Name + " spawned");
+            GameObject spawned = Instantiate(alimentToSpawn.Prefab, transform.position, transform.rotation);
+
+            var controller = spawned.GetComponent<AlimentsController>();
+            if (controller != null)
+            {
+                controller.SetIndex(alimentToSpawn.Index);
+                controller.SetAlimentsData(aliments);
+            }
+
+            Debug.Log($"{alimentToSpawn.Name} spawned");
         }
     }
 }
