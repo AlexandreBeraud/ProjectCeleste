2a4b4699569c07ef538488e8587eb81095e79efe
AlexandreBeraud <beraud.mailpro@gmail.com>
2025-10-29 09:47:45 +0100

    POWER UP ! BOMB

--- Assets/Scripts/PowerUps/BombPowerUp.cs
+++ Assets/Scripts/PowerUps/BombPowerUp.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 public class BombPowerUp : MonoBehaviour
 {
     [SerializeField] private BombScriptableObject bombData;
-    
     [SerializeField] private BoxCollider boxCollider;
     [SerializeField] private ParticleSystem explosionParticleSystem;
-    
+
     [Header("DEBUG")]
     [SerializeField] private int bombLevel;
-    
+
     private void OnCollisionEnter(Collision collision)
     {
         Explode();
@@ -19,9 +18,17 @@ public class BombPowerUp : MonoBehaviour
     private void Explode()
     {
         var bombInfo = bombData.GetBombByLevel(bombLevel);
-        
+        if (bombInfo == null)
+        {
+            Debug.LogWarning($"No bomb info found for level {bombLevel}");
+            return;
+        }
+
+        // Effets visuels et sonores
         Instantiate(explosionParticleSystem, transform.position, Quaternion.identity);
-        AudioSource.PlayClipAtPoint(bombInfo.DestroySound, transform.position);
+
+        if (bombInfo.DestroySound != null)
+            AudioSource.PlayClipAtPoint(bombInfo.DestroySound, transform.position);
 
         DestroyAliments(bombInfo);
         Destroy(gameObject);
@@ -30,32 +37,46 @@ public class BombPowerUp : MonoBehaviour
     private void DestroyAliments(BombScriptableObject.Bomb bombInfo)
     {
         int alimentsToDestroy = Mathf.Clamp(bombLevel, 1, 3);
-        
-        Collider[] colliders = Physics.OverlapBox(transform.position, boxCollider.size);
-        
-        List<Collider> nearbyAliments = new List<Collider>(colliders);
-        nearbyAliments.Sort((a,b) => 
-            Vector3.Distance(a.transform.position, transform.position)
-                .CompareTo(Vector3.Distance(transform.position, b.transform.position)
-        ));
 
+        Vector3 halfExtents = boxCollider.size * 1f;
+        halfExtents.Scale(transform.localScale);
+
+        Collider[] colliders = Physics.OverlapBox(transform.position, halfExtents, Quaternion.identity);
+
+        List<Collider> nearbyAliments = new List<Collider>();
+        foreach (var col in colliders)
+        {
+            if (col.CompareTag("Aliments"))
+                nearbyAliments.Add(col);
+        }
+
+        nearbyAliments.Sort((a, b) =>
+            Vector3.Distance(a.transform.position, transform.position)
+                .CompareTo(Vector3.Distance(b.transform.position, transform.position))
+        );
+
+        int destroyedCount = 0;
         for (int i = 0; i < Mathf.Min(alimentsToDestroy, nearbyAliments.Count); i++)
         {
             GameObject aliment = nearbyAliments[i].gameObject;
-            Destroy(aliment);
+            if (aliment != null)
+            {
+                Destroy(aliment);
+                destroyedCount++;
+            }
         }
-        
-        Debug.Log($"Bomb Level : {bombInfo.Level} ; Destroyed " + alimentsToDestroy);
+
+        Debug.Log($"Bomb Level {bombInfo.Level} destroyed {destroyedCount}/{alimentsToDestroy} aliments");
     }
 
-
-    public void SetBombLevel(int level)
+    public void SetBombLevel(int level) => bombLevel = Mathf.Clamp(level, 1, 3);
+    public void SetBombData(BombScriptableObject data) => bombData = data;
+    
+    private void OnDrawGizmosSelected()
     {
-        bombLevel = Mathf.Clamp(level, 1, 3);
-    }
-
-    public void SetBombData(BombScriptableObject data)
-    {
-        bombData = data;
+        Gizmos.color = new Color(1, 0, 0, 0.3f);
+        Vector3 halfExtents = boxCollider.size * 0.5f;
+        halfExtents.Scale(transform.localScale);
+        Gizmos.DrawCube(transform.position, halfExtents * 2);
     }
 }
